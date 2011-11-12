4a64cb7cc52ff576f2807aaa910b70ff9aac7881
Praveen Seshadri <praveen_seshadri@hotmail.com>
2011-11-11 16:53:08 -0800

    initial commit

---
+++ WcfRestServiceSample/JSONHelper.js
@@ -0,0 +1,39 @@
+﻿$.postJSON = function (url, data, callback) {
+    $.ajax({
+        'url': url,
+        'type': 'post',
+        'processData': false,
+        'data': JSON.stringify(data),
+        contentType: 'application/json',
+        success: function (data) { if (null != callback) { callback(data); } }
+    });
+};
+
+
+function GetTabString(tabLevel) {
+    var returnString = '';
+    for (i = 0; i < tabLevel; i++) {
+        returnString += '\t';
+    }
+    return returnString;
+};
+
+
+$.printableJSON = function (JSONObject, tabLevel) {
+    var printString = "";
+    if (typeof JSONObject == 'object') {
+        printString += "{ \n";
+        $.each(JSONObject, function (key, value) {
+            printString += GetTabString(tabLevel) + key + ":\t" + $.printableJSON(value, tabLevel + 1) + "\n";
+        });
+        printString += GetTabString(tabLevel) + " } \n";
+    }
+    else {
+        printString += JSONObject;
+    }
+    return printString;
+};
+
+
+
+
